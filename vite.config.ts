8fa19b43e28e0c9a2eeb842e4e06736055877de8
sanadxX <brawlstarzsnd@gmail.com>
2026-06-20 02:41:01 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: '/web/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
