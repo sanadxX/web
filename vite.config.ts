2737cb82a4ec6a25fab4831ef6c4030e7e115725
sanadxX <brawlstarzsnd@gmail.com>
2026-06-20 02:52:41 +0300

    Fix indentation in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/web/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: '/web/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
